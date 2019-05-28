0c5971a65a01d84cf9005dcc3a73f07a80d0b04d
Jordan Heffernan <jheff2@gmail.com>
2019-05-28 14:30:17 -0700

    first pass at contacts table fields

--- db/migrations/development/20190528114029_create_contacts_table.js
+++ db/migrations/development/20190528114029_create_contacts_table.js
@@ -1,14 +1,55 @@
 exports.up = function(knex, Promise) {
   return Promise.all([
     knex.schema.createTable("contacts", function(table) {
-      table.uuid("id").primary();
-      table.string("name").notNullable();
-      table.string("email").notNullable();
-      table.string("avatar_url");
-      table.string("google_id").notNullable();
-      table.string("google_token").notNullable();
-      table.timestamp("created_at").defaultTo(knex.fn.now());
-      table.timestamp("updated_at").defaultTo(knex.fn.now());
+      table.uuid("contact_id").primary();
+      table.string("display_name").notNullable();
+      table.string("account_name").notNullable();
+      table.string("agency_number").notNullable();
+      table
+        .string("mail_to_city")
+        .notNullable()
+        .defaultTo("OR");
+      table.string("mail_to_state").notNullable();
+      table.string("mail_to_street").notNullable();
+      table.string("mail_to_postal_code", 5).notNullable();
+      table.string("first_name").notNullable();
+      table.string("last_name").notNullable();
+      table.string("last_name").notNullable();
+      table.string("dd", 2).notNullable();
+      table.string("mm", 2).notNullable();
+      table.string("yyyy", 4).notNullable();
+      table.date("dob").notNullable();
+      table
+        .string("preferred_language")
+        .notNullable()
+        .defaultTo("english");
+      table.string("home_street").notNullable();
+      table.string("home_postal_code", 5).notNullable();
+      table
+        .string("home_state")
+        .notNullable()
+        .defaultTo("OR");
+      table.string("home_city").notNullable();
+      table.string("home_email").notNullable();
+      table.string("mobile_phone").notNullable();
+      table.boolean("text_auth_opt_out").notNullable();
+      table.boolean("terms_agree").notNullable();
+      table.string("signature").notNullable();
+      table.string("online_campaign_source");
+      table.boolean("signed_application").notNullable();
+      table.string("ethnicity");
+      table.boolean("lgbtq_id");
+      table.boolean("trans_id");
+      table.boolean("disability_id");
+      table.boolean("deaf_or_hard_of_hearing");
+      table.boolean("blind_or_visually_impaired");
+      table.string("gender");
+      table.string("gender_other_description");
+      table.string("gender_pronoun");
+      table.string("job_title").notNullable();
+      table.date("hire_date").notNullable();
+      table.string("worksite").notNullable();
+      table.string("work_email").notNullable();
     })
   ]);
 };
