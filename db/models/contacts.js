6b06fc8a6d06262a29b67d6ffbffcade51af2e19
Jordan Heffernan <jheff2@gmail.com>
2019-05-29 15:52:20 -0700

    changed 'id' to 'contact_id' in public functions to match db column name'

--- db/models/contacts.js
+++ db/models/contacts.js
@@ -8,44 +8,44 @@ const { db, TABLES } = require("../../app/config/knex");
 /* ============================ PUBLIC METHODS ============================= */
 
 /** Create a Contact
- * @param  {String} display_name                  New contact display_name
- * @param  {String} account_name                  New contact account_name
- * @param  {String} agency_number                 New contact agency_number
- * @param  {String} mail_to_city                  New contact mail_to_city
- * @param  {String} mail_to_state                 New contact mail_to_state
- * @param  {String} mail_to_street                New contact mail_to_street
- * @param  {String} mail_to_postal_code           New contact mail_to_postal_code
- * @param  {String} first_name                    New contact first_name
- * @param  {String} last_name                     New contact last_name
- * @param  {String} dd                            New contact dd
- * @param  {String} mm                            New contact mm
- * @param  {String} yyyy                          New contact yyyy
- * @param  {String} dob                           New contact dob
- * @param  {String} preferred_language            New contact preferred_language
- * @param  {String} home_street                   New contact home_street
- * @param  {String} home_postal_code              New contact home_postal_code
- * @param  {String} home_state                    New contact home_state
- * @param  {String} home_city                     New contact home_city
- * @param  {String} home_email                    New contact home_email
- * @param  {String} mobile_phone                  New contact mobile_phone
- * @param  {String} text_auth_opt_out             New contact text_auth_opt_out
- * @param  {String} terms_agree                   New contact terms_agree
- * @param  {String} signature                     New contact signature
- * @param  {String} online_campaign_source        New contact online_campaign_source
- * @param  {String} signed_application            New contact signed_application
- * @param  {String} ethnicity                     New contact ethnicity
- * @param  {String} lgbtq_id                      New contact lgbtq_id
- * @param  {String} trans_id                      New contact trans_id
- * @param  {String} disability_id                 New contact disability_id
- * @param  {String} deaf_or_hard_of_hearing       New contact deaf_or_hard_of_hearing
- * @param  {String} blind_or_visually_impaired    New contact blind_or_visually_impaired
- * @param  {String} gender                        New contact gender
- * @param  {String} gender_other_description      New contact gender_other_description
- * @param  {String} gender_pronoun                New contact gender_pronoun
- * @param  {String} job_title                     New contact job_title
- * @param  {String} hire_date                     New contact hire_date
- * @param  {String} worksite                      New contact worksite
- * @param  {String} work_email                    New contact work_email
+ * @param    {String}   display_name                  New contact display_name
+ * @param    {String}   account_name                  New contact account_name
+ * @param    {String}   agency_number                 New contact agency_number
+ * @param    {String}   mail_to_city                  New contact mail_to_city
+ * @param    {String}   mail_to_state                 New contact mail_to_state
+ * @param    {String}   mail_to_street                New contact mail_to_street
+ * @param    {String}   mail_to_postal_code           New contact mail_to_postal_code
+ * @param    {String}   first_name                    New contact first_name
+ * @param    {String}   last_name                     New contact last_name
+ * @param    {String}   dd                            New contact dd
+ * @param    {String}   mm                            New contact mm
+ * @param    {String}   yyyy                          New contact yyyy
+ * @param    {String}   dob                           New contact dob
+ * @param    {String}   preferred_language            New contact preferred_language
+ * @param    {String}   home_street                   New contact home_street
+ * @param    {String}   home_postal_code              New contact home_postal_code
+ * @param    {String}   home_state                    New contact home_state
+ * @param    {String}   home_city                     New contact home_city
+ * @param    {String}   home_email                    New contact home_email
+ * @param    {String}   mobile_phone                  New contact mobile_phone
+ * @param    {String}   text_auth_opt_out             New contact text_auth_opt_out
+ * @param    {String}   terms_agree                   New contact terms_agree
+ * @param    {String}   signature                     New contact signature
+ * @param    {String}   online_campaign_source        New contact online_campaign_source
+ * @param    {String}   signed_application            New contact signed_application
+ * @param    {String}   ethnicity                     New contact ethnicity
+ * @param    {String}   lgbtq_id                      New contact lgbtq_id
+ * @param    {String}   trans_id                      New contact trans_id
+ * @param    {String}   disability_id                 New contact disability_id
+ * @param    {String}   deaf_or_hard_of_hearing       New contact deaf_or_hard_of_hearing
+ * @param    {String}   blind_or_visually_impaired    New contact blind_or_visually_impaired
+ * @param    {String}   gender                        New contact gender
+ * @param    {String}   gender_other_description      New contact gender_other_description
+ * @param    {String}   gender_pronoun                New contact gender_pronoun
+ * @param    {String}   job_title                     New contact job_title
+ * @param    {String}   hire_date                     New contact hire_date
+ * @param    {String}   worksite                      New contact worksite
+ * @param    {String}   work_email                    New contact work_email
  * @returns {Array} Array of 1 newly-created Contact Object.
  */
 const createContact = (
@@ -90,7 +90,7 @@ const createContact = (
 ) => {
   return db
     .insert({
-      id: uuid.v4(),
+      contact_id: uuid.v4(),
       display_name,
       account_name,
       agency_number,
@@ -135,51 +135,51 @@ const createContact = (
 };
 
 /** Update a Contact
- *  @param    {String}   id             The id of the user to update.
+ *  @param    {String}   contact_id     The id of the contact to update.
  *  @param    {Object}   updates        Key/value pairs of fields to update.
- ****  @param  {String} display_name                  Updated display_name
- ****  @param  {String} account_name                  Updated account_name
- ****  @param  {String} agency_number                 Updated agency_number
- ****  @param  {String} mail_to_city                  Updated mail_to_city
- ****  @param  {String} mail_to_state                 Updated mail_to_state
- ****  @param  {String} mail_to_street                Updated mail_to_street
- ****  @param  {String} mail_to_postal_code           Updated mail_to_postal_code
- ****  @param  {String} first_name                    Updated first_name
- ****  @param  {String} last_name                     Updated last_name
- ****  @param  {String} dd                            Updated dd
- ****  @param  {String} mm                            Updated mm
- ****  @param  {String} yyyy                          Updated yyyy
- ****  @param  {String} dob                           Updated dob
- ****  @param  {String} preferred_language            Updated preferred_language
- ****  @param  {String} home_street                   Updated home_street
- ****  @param  {String} home_postal_code              Updated home_postal_code
- ****  @param  {String} home_state                    Updated home_state
- ****  @param  {String} home_city                     Updated home_city
- ****  @param  {String} home_email                    Updated home_email
- ****  @param  {String} mobile_phone                  Updated mobile_phone
- ****  @param  {String} text_auth_opt_out             Updated text_auth_opt_out
- ****  @param  {String} terms_agree                   Updated terms_agree
- ****  @param  {String} signature                     Updated signature
- ****  @param  {String} online_campaign_source        Updated online_campaign_source
- ****  @param  {String} signed_application            Updated signed_application
- ****  @param  {String} ethnicity                     Updated ethnicity
- ****  @param  {String} lgbtq_id                      Updated lgbtq_id
- ****  @param  {String} trans_id                      Updated trans_id
- ****  @param  {String} disability_id                 Updated disability_id
- ****  @param  {String} deaf_or_hard_of_hearing       Updated deaf_or_hard_of_hearing
- ****  @param  {String} blind_or_visually_impaired    Updated blind_or_visually_impaired
- ****  @param  {String} gender                        Updated gender
- ****  @param  {String} gender_other_description      Updated gender_other_description
- ****  @param  {String} gender_pronoun                Updated gender_pronoun
- ****  @param  {String} job_title                     Updated job_title
- ****  @param  {String} hire_date                     Updated hire_date
- ****  @param  {String} worksite                      Updated worksite
- ****  @param  {String} work_email                    Updated work_email
+ ****  @param    {String}   display_name                  Updated display_name
+ ****  @param    {String}   account_name                  Updated account_name
+ ****  @param    {String}   agency_number                 Updated agency_number
+ ****  @param    {String}   mail_to_city                  Updated mail_to_city
+ ****  @param    {String}   mail_to_state                 Updated mail_to_state
+ ****  @param    {String}   mail_to_street                Updated mail_to_street
+ ****  @param    {String}   mail_to_postal_code           Updated mail_to_postal_code
+ ****  @param    {String}   first_name                    Updated first_name
+ ****  @param    {String}   last_name                     Updated last_name
+ ****  @param    {String}   dd                            Updated dd
+ ****  @param    {String}   mm                            Updated mm
+ ****  @param    {String}   yyyy                          Updated yyyy
+ ****  @param    {String}   dob                           Updated dob
+ ****  @param    {String}   preferred_language            Updated preferred_language
+ ****  @param    {String}   home_street                   Updated home_street
+ ****  @param    {String}   home_postal_code              Updated home_postal_code
+ ****  @param    {String}   home_state                    Updated home_state
+ ****  @param    {String}   home_city                     Updated home_city
+ ****  @param    {String}   home_email                    Updated home_email
+ ****  @param    {String}   mobile_phone                  Updated mobile_phone
+ ****  @param    {String}   text_auth_opt_out             Updated text_auth_opt_out
+ ****  @param    {String}   terms_agree                   Updated terms_agree
+ ****  @param    {String}   signature                     Updated signature
+ ****  @param    {String}   online_campaign_source        Updated online_campaign_source
+ ****  @param    {String}   signed_application            Updated signed_application
+ ****  @param    {String}   ethnicity                     Updated ethnicity
+ ****  @param    {String}   lgbtq_id                      Updated lgbtq_id
+ ****  @param    {String}   trans_id                      Updated trans_id
+ ****  @param    {String}   disability_id                 Updated disability_id
+ ****  @param    {String}   deaf_or_hard_of_hearing       Updated deaf_or_hard_of_hearing
+ ****  @param    {String}   blind_or_visually_impaired    Updated blind_or_visually_impaired
+ ****  @param    {String}   gender                        Updated gender
+ ****  @param    {String}   gender_other_description      Updated gender_other_description
+ ****  @param    {String}   gender_pronoun                Updated gender_pronoun
+ ****  @param    {String}   job_title                     Updated job_title
+ ****  @param    {String}   hire_date                     Updated hire_date
+ ****  @param    {String}   worksite                      Updated worksite
+ ****  @param    {String}   work_email                    Updated work_email
  *  @returns  {Object}   Updated Contact object.
  */
-const updateContact = (id, updates) => {
+const updateContact = (contact_id, updates) => {
   return db(TABLES.CONTACTS)
-    .where({ id })
+    .where({ contact_id })
     .first()
     .update(updates)
     .update("updated_at", db.fn.now())
@@ -195,25 +195,25 @@ const getContacts = () => {
 };
 
 /** Find a Contact by id
- *  @param    {String}   id   The id of the contact to delete.
+ *  @param    {String}   contact_id   The id of the contact to delete.
  *  @returns  success message
  */
 
-const getContactById = id => {
+const getContactById = contact_id => {
   return db(TABLES.CONTACTS)
-    .where({ id })
+    .where({ contact_id })
     .first()
     .returning("*");
 };
 
 /** Delete Contact
- *  @param    {String}   id   The id of the Contact to delete.
+ *  @param    {String}   contact_id   The id of the Contact to delete.
  *  @returns  success message
  */
 
-const deleteContact = id => {
+const deleteContact = contact_id => {
   return db(TABLES.CONTACTS)
-    .where({ id })
+    .where({ contact_id })
     .del()
     .then(() => {
       // then return success message to client
