4ed89177d3d8e9517db0059ec42db8698c8ea477
rifkegribenes <rifkegribenes@gmail.com>
2019-06-04 15:07:36 -0700

    image uploading working

--- app/controllers/imageUpload.ctrl.js
+++ app/controllers/imageUpload.ctrl.js
@@ -4,6 +4,9 @@
 
 /* ================================= SETUP ================================= */
 
+// import model
+const formMeta = require("../../db/models/form_meta");
+
 const aws = require("aws-sdk");
 const multerS3 = require("multer-s3");
 const multer = require("multer");
@@ -16,18 +19,21 @@ const s3 = new aws.S3(s3config);
 /**
  * Single Upload
  */
-const imgUploadMulter = multer({
+const upload = multer({
   storage: multerS3({
     s3: s3,
     bucket: s3config.bucket,
     acl: "public-read",
     key: function(req, file, cb) {
+      console.log(file);
       cb(
         null,
-        path.basename(file.originalname, path.extname(file.originalname)) +
-          "-" +
-          Date.now() +
-          path.extname(file.originalname)
+        path.basename(
+          file.originalname,
+          `${path.extname(file.originalname)}-${Date.now()}${path.extname(
+            file.originalname
+          )}`
+        )
       );
     }
   }),
@@ -36,6 +42,18 @@ const imgUploadMulter = multer({
     checkFileType(file, cb);
   }
 }).single("image");
+
+// const upload = multer({
+//     storage: multerS3({
+//         s3: s3,
+//         acl: 'public-read',
+//         bucket: s3config.bucket,
+//         key: function (req, file, cb) {
+//             console.log(file);
+//             cb(null, file.originalname); //use Date.now() for unique file keys
+//         }
+//     })
+// }).single('image');
 
 /**
  * Check File Type
@@ -63,21 +81,65 @@ const checkFileType = (file, cb) => {
  */
 const singleImgUpload = (req, res, next) => {
   console.log("singleImgUpload");
-  console.log(req.body);
-  console.log(req.error);
-  if (req.body.file && req.body.file !== undefined && !req.error) {
-    const imageName = req.body.file.key;
-    const imageLocation = req.body.file.location;
 
-    // Return file name and location
-    res.status(201).json({
-      image: imageName,
-      location: imageLocation
-    });
-  } else {
-    console.log(`imageUpload.ctrl > 76`);
-    res.status(500).json({ message: "Error: Please select a file to upload" });
-  }
+  upload(req, res, err => {
+    if (err) {
+      console.log(err);
+      res.status(500).json({
+        message: err
+      });
+    } else {
+      const imageUrl = `https://member-app-images.s3-us-west-2.amazonaws.com/${
+        req.file.originalname
+      }`;
+      console.log();
+      res.status(200).json({
+        message: "Successful upload",
+        imageUrl: imageUrl
+      });
+    }
+  });
+
+  // .then((returnVal) => {
+  //   console.log('upload complete');
+  //   console.log(returnVal);
+  //   res.status(200).json({
+  //     message: 'Successful upload',
+  //     returnVal: returnVal
+  //   });
+  // })
+  // .catch(err => {
+  //   console.log(err);
+  //   res.status(500).json({
+  //     message: err
+  //   });
+  // });
+
+  // if (req.body.file && req.body.file !== undefined && !req.error) {
+  //   const imageName = req.body.file.key;
+  //   const imageLocation = req.body.file.location;
+
+  //   return formMeta
+  //     .createFormMeta("image", imageLocation)
+  //     .then(records => {
+  //       const record = records[0];
+  //       res.status(200).json(record);
+  //     })
+  //     .catch(err => {
+  //       console.log(`formMeta.ctrl.js > 28: ${err}`);
+  //       res.status(500).json({ message: err.message });
+  //     });
+
+  //   // Return file name and location
+  //   res.status(201).json({
+  //     image: imageName,
+  //     location: imageLocation
+  //   });
+  // } else {
+  //   console.log(`imageUpload.ctrl > 76`);
+  //   console.log(res);
+  //   res.status(500).json({ message: "Error: Please select a file to upload" });
+  // }
 };
 
 /* ================================ EXPORT ================================= */
