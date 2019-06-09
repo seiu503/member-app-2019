8ddd77bf46ae8d7b83210f5aa37017cb20fb2f0d
rifkegribenes <rifkegribenes@gmail.com>
2019-06-08 22:35:26 -0700

    alert dialog tests

--- client/src/__tests__/components/AlertDialog.test.js
+++ client/src/__tests__/components/AlertDialog.test.js
@@ -3,21 +3,41 @@ import { shallow, mount } from "enzyme";
 import { unwrap } from "@material-ui/core/test-utils";
 import MuiThemeProvider from "@material-ui/core/styles/MuiThemeProvider";
 import AlertDialog from "../../components/AlertDialog";
+import Dialog from "@material-ui/core/Dialog";
+import Button from "@material-ui/core/Button";
 import theme from "../../styles/theme";
 
 const AlertDialogNaked = unwrap(AlertDialog);
 
+const props = {
+  open: true,
+  title: "Dialog title",
+  content: "Dialog content",
+  buttonText: "Do the action",
+  handleClose: jest.fn(),
+  action: jest.fn(),
+  classes: {}
+};
+
 describe("<AlertDialog />", () => {
-  it("renders with shallow", () => {
-    shallow(<AlertDialogNaked classes={{}} open={false} />);
+  it("renders without crashing", () => {
+    shallow(<AlertDialogNaked {...props} />);
   });
 
-  it("renders with mount", () => {
-    mount(
-      <MuiThemeProvider theme={theme}>
-        <AlertDialog open={false} />
-      </MuiThemeProvider>
+  it("renders a title if passed a title prop", () => {
+    const titleEl = shallow(<AlertDialogNaked {...props} />).find(
+      "#alert-dialog-title"
     );
+    expect(titleEl.exists()).toEqual(true);
+    expect(titleEl.text()).toEqual(props.title);
+  });
+
+  it("doesn't render a title if no title prop", () => {
+    props.title = undefined;
+    const titleEl = shallow(<AlertDialogNaked {...props} />).find(
+      "#alert-dialog-title"
+    );
+    expect(titleEl.exists()).toEqual(false);
   });
 });
 
