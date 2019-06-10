a922e74465d5b149ccc441c0410de613008ec81b
rifkegribenes <rifkegribenes@gmail.com>
2019-06-10 16:38:00 -0700

    footer and not found tests

--- client/src/__tests__/components/NotFound.test.js
+++ client/src/__tests__/components/NotFound.test.js
@@ -1,9 +1,27 @@
 import React from "react";
 import { shallow } from "enzyme";
+import { findByTestAttr } from "../../utils/testUtils";
 import NotFound from "../../components/NotFound";
 
+const defaultProps = {
+  classes: {}
+};
+
+/**
+ * Factory function to create a ShallowWrapper for the NotFound component
+ * @function setup
+ * @param  {object} props - Component props specific to this setup.
+ * @return {ShallowWrapper}
+ */
+const setup = (props = {}) => {
+  const setupProps = { ...defaultProps, ...props };
+  return shallow(<NotFound {...setupProps} />);
+};
+
 describe("<NotFound />", () => {
-  it("renders without crashing", () => {
-    shallow(<NotFound classes={{}} />);
+  it("renders without error", () => {
+    const wrapper = setup();
+    const component = findByTestAttr(wrapper, "component-not-found");
+    expect(component.length).toBe(1);
   });
 });
