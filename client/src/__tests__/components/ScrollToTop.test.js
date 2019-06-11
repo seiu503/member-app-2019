4f020acf2a04108cbd1e95e36fb288558da16f97
rifkegribenes <rifkegribenes@gmail.com>
2019-06-10 19:42:29 -0700

    scroll to top tests

--- client/src/__tests__/components/ScrollToTop.test.js
+++ client/src/__tests__/components/ScrollToTop.test.js
@@ -1,10 +1,37 @@
 import React from "react";
-import { shallow } from "enzyme";
-import ScrollToTop from "../../components/ScrollToTop";
+import { shallow, mount } from "enzyme";
+import { BrowserRouter as Router } from "react-router-dom";
+import ScrollToTop, {
+  UnwrappedScrollToTop
+} from "../../components/ScrollToTop";
+
+const ScrollToMock = jest.fn();
 
 describe("<ScrollToTop />", () => {
+  beforeAll(() => {
+    // mock window.scrollTo function
+    global.window.scrollTo = ScrollToMock;
+  });
+  afterAll(() => {
+    // cleanup after testing
+    ScrollToMock.mockClear();
+  });
   it("renders without error", () => {
     const wrapper = shallow(<ScrollToTop />);
     expect(wrapper.length).toBe(1);
+  });
+  it("calls window.scrollTo if props.location changes", () => {
+    const wrapper = mount(
+      <UnwrappedScrollToTop children="" location={{ pathname: "dashboard" }} />
+    );
+
+    // pass different location prop to trigger componentDidUpdate
+    wrapper.setProps({ location: { pathname: "library" } });
+
+    // check to see if scrollToMock was called in componentDidUpdate
+    expect(ScrollToMock).toHaveBeenCalledWith({
+      top: 0,
+      behavior: "smooth"
+    });
   });
 });
