17b7304ca4a22296c325db927a645dae2165333a
rifkegribenes <rifkegribenes@gmail.com>
2019-06-10 17:00:41 -0700

    spinner and sinner adornment tests

--- client/src/__tests__/components/SpinnerAdornment.test.js
+++ client/src/__tests__/components/SpinnerAdornment.test.js
@@ -3,9 +3,10 @@ import { shallow } from "enzyme";
 import { SpinnerAdornment } from "../../components/SpinnerAdornment";
 import CircularProgress from "@material-ui/core/CircularProgress";
 
-describe("<Spinner />", () => {
-  it("renders without crashing", () => {
-    shallow(<SpinnerAdornment classes={{}} />);
+describe("<SpinnerAdornment />", () => {
+  it("renders without error", () => {
+    const wrapper = shallow(<SpinnerAdornment classes={{}} />);
+    expect(wrapper.length).toBe(1);
   });
 
   it("should render a CircularProgress", () => {
