55dcbfe6e8997e51c5ba0b8fb6ef2cf3a9100cc4
Jordan Heffernan <jheff2@gmail.com>
2019-06-28 14:44:48 -0700

    test setup for helper functions.

---
+++ client/src/__tests__/components/SubmissionFormElements.test.js
@@ -0,0 +1,28 @@
+import React from "react";
+import { shallow } from "enzyme";
+
+import * as formElements from "../components/SubmissionFormElements";
+
+describe("Helper Functions", () => {
+  describe("getMaxDay", () => {
+    it("returns correct max day given any possible month", () => {});
+  });
+
+  describe("dateOptions", () => {
+    it("recieves correct max from getMaxDay", () => {});
+
+    it("returns an array of all possible dates for given month", () => {});
+  });
+
+  describe("yearOptions", () => {
+    it("returns array of all years for last 110 years", () => {});
+  });
+});
+
+describe("Input Field Render functions", () => {
+  describe("renderTextField", () => {});
+
+  describe("renderSelect", () => {});
+
+  describe("renderCheckbox", () => {});
+});
