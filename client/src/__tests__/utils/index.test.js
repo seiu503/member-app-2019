715166db901ba12285b5ef0b953c351d378f896b
rifkegribenes <rifkegribenes@gmail.com>
2019-07-09 14:57:03 -0700

    utils tests

---
+++ client/src/__tests__/utils/index.test.js
@@ -0,0 +1,18 @@
+import * as utils from "../../utils";
+
+describe("utils/index", () => {
+  it("`formatDate` formats dates correctly", () => {
+    const date = utils.formatDate(new Date("7/7/2019"));
+    expect(date).toBe("Jul 7, 2019");
+  });
+  it("`randomInt` generates a random number between 100 and 999", () => {
+    const number = utils.randomInt();
+    expect(number).toBeGreaterThan(100);
+    expect(number).toBeLessThan(999);
+  });
+  it("`skip` switches focus to targeted element", () => {
+    const dummyElement = document.createElement("div");
+    document.getElementById = jest.fn().mockImplementation(() => dummyElement);
+    utils.skip("div");
+  });
+});
