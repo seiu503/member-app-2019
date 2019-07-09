9fc8a2c0540d55d6194b49a982595022030a3bde
rifkegribenes <rifkegribenes@gmail.com>
2019-07-09 15:56:25 -0700

    sf api call working

--- app/controllers/sf.ctrl.js
+++ app/controllers/sf.ctrl.js
@@ -14,7 +14,9 @@ const fieldList = generateSFContactFieldList();
  */
 const getSFContactById = (req, res, next) => {
   const { id } = req.params;
-  const query = `SELECT ${fieldList.join(",")} FROM Contact WHERE Id = ${id}`;
+  const query = `SELECT ${fieldList.join(
+    ","
+  )} FROM Contact WHERE Id = \'${id}\'`;
   conn.login(user, password, function(err, userInfo) {
     if (err) {
       return console.error(err);
@@ -25,15 +27,19 @@ const getSFContactById = (req, res, next) => {
     console.log("User ID: " + userInfo.id);
     console.log("Org ID: " + userInfo.organizationId);
 
-    conn.query(query, function(err, contact) {
-      if (err) {
-        return console.error(err);
-      }
-      console.log("total : " + contact.totalSize);
-      console.log("fetched : " + contact.records.length);
-      console.log(contact);
-      res.status(200).json(contact);
-    });
+    try {
+      conn.query(query, function(err, contact) {
+        if (err) {
+          return console.error(err);
+        }
+        console.log("total : " + contact.totalSize);
+        console.log("fetched : " + contact.records.length);
+        console.log(contact);
+        res.status(200).json(contact.records[0]);
+      });
+    } catch (err) {
+      return console.error(err);
+    }
   });
 };
 
