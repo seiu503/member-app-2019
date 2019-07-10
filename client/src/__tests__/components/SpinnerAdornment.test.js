93d43be9ed43052aa681a6ce8cfe39dca281f4fc
rifkegribenes <rifkegribenes@gmail.com>
2019-07-10 16:40:29 -0700

    fix spinner adornment tests

--- client/src/__tests__/components/SpinnerAdornment.test.js
+++ client/src/__tests__/components/SpinnerAdornment.test.js
@@ -1,9 +1,7 @@
 import React from "react";
-import { shallow, mount } from "enzyme";
+import { shallow } from "enzyme";
 import SpinnerAdornment, {
-  SpinnerAdornmentBase,
-  withStylesProps,
-  styles
+  SpinnerAdornmentBase
 } from "../../components/SpinnerAdornment";
 import CircularProgress from "@material-ui/core/CircularProgress";
 
@@ -16,41 +14,5 @@ describe("<SpinnerAdornment />", () => {
   it("renders a CircularProgress", () => {
     const wrapper = shallow(<SpinnerAdornmentBase classes={{}} />);
     expect(wrapper.find(CircularProgress)).toHaveLength(1);
-  });
-
-  it("assigns correct color to spinner (`variant: raised`)", () => {
-    const props = { variant: "raised", color: "primary" };
-    const getContrastTextMock = jest.fn();
-    const theme = {
-      palette: {
-        getContrastText: getContrastTextMock,
-        primary: {
-          main: "black"
-        }
-      }
-    };
-    getContrastTextMock.mockReturnValueOnce("black");
-    const spinnerColor = styles(props)(theme);
-    expect(getContrastTextMock.mock.calls.length).toBe(1);
-    getContrastTextMock.mockRestore();
-    expect(spinnerColor.spinner.color).toBe("black");
-  });
-
-  it("assigns correct color to spinner (`variant !== raised`)", () => {
-    const props = { variant: null, color: "primary" };
-    const getContrastTextMock = jest.fn();
-    const theme = {
-      palette: {
-        getContrastText: getContrastTextMock,
-        primary: {
-          main: "black"
-        }
-      }
-    };
-    getContrastTextMock.mockReturnValueOnce("#ffffff");
-    const spinnerColor = styles(props)(theme);
-    expect(getContrastTextMock.mock.calls.length).toBe(1);
-    getContrastTextMock.mockRestore();
-    expect(spinnerColor.spinner.color).toBe("#ffffff");
   });
 });
