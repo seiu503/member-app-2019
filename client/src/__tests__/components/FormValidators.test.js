e369c0b06f26d8c737b6702a394294ae6a217fdb
Jordan Heffernan <jheff2@gmail.com>
2019-07-18 16:43:21 -0700

    added more comprehensive email and phone validator tests

--- client/src/__tests__/components/FormValidators.test.js
+++ client/src/__tests__/components/FormValidators.test.js
@@ -23,12 +23,32 @@ describe("Redux-Form custom Validators", () => {
     expect(validate(testData)).toStrictEqual({
       mobilePhone: "Invalid phone number (e.g. 555-123-456)"
     });
+    testData.mobilePhone = "phone number";
+    expect(validate(testData)).toStrictEqual({
+      mobilePhone: "Invalid phone number (e.g. 555-123-456)"
+    });
+    testData.mobilePhone = "5555555555";
+    expect(validate(testData)).toStrictEqual({});
+    testData.mobilePhone = "(503) 555-5555";
+    expect(validate(testData)).toStrictEqual({});
+    testData.mobilePhone = "555.555.5555";
+    expect(validate(testData)).toStrictEqual({});
+    testData.mobilePhone = "555-555-5555";
+    expect(validate(testData)).toStrictEqual({});
+    testData.mobilePhone = "555 555 5555";
+    expect(validate(testData)).toStrictEqual({});
   });
   test("validates properly formed emails", () => {
     testData.homeEmail = "fake@email";
     expect(validate(testData)).toStrictEqual({
       homeEmail: "Invalid email address (e.g. sample@email.com)"
     });
+    testData.homeEmail = "fake@email.co";
+    expect(validate(testData)).toStrictEqual({});
+    testData.homeEmail = "fake@email.co";
+    expect(validate(testData)).toStrictEqual({});
+    testData.homeEmail = "f@g.org";
+    expect(validate(testData)).toStrictEqual({});
   });
   test("validates properly formed zip codes", () => {
     testData.homePostalCode = 4444;
