613e9d3132814330bd51adad2a7cd8d2d68c0338
Jordan Heffernan <jheff2@gmail.com>
2019-07-25 12:28:41 -0700

    adjusting test env migrations

--- db/migrations/testing/20190603101823_create_submissions_test_table.js
+++ db/migrations/testing/20190603101823_create_submissions_test_table.js
@@ -1,38 +1,61 @@
 exports.up = function(knex, Promise) {
   return Promise.all([
-    knex.schema.createTable("submissions", function(table) {
-      table.uuid("submission_id").primary();
-      table.string("ip_address").notNullable();
-      table.date("submission_date").notNullable();
-      table.string("agency_number");
-      table.date("birthdate").notNullable();
-      table.string("cell_phone").notNullable();
-      table.string("employer_name").notNullable();
-      table.string("first_name").notNullable();
-      table.string("last_name").notNullable();
-      table.string("home_street").notNullable();
-      table.string("home_city").notNullable();
-      table.string("home_state").notNullable();
-      table.string("home_zip").notNullable();
-      table.string("home_email").notNullable();
-      table.string("preferred_language").notNullable();
-      table.string("terms_agree").notNullable();
-      table.string("signature").notNullable();
-      table.string("text_auth_opt_out");
-      table.string("online_campaign_source");
-      table.string("contact_id");
-      table.string("legal_language").notNullable();
-      table.date("maintenance_of_effort").notNullable();
-      table.date("seiu503_cba_app_date").notNullable();
-      table.date("direct_pay_auth");
-      table.date("direct_deposit_auth");
-      table.string("immediate_past_member_status");
-      table.timestamp("created_at").defaultTo(knex.fn.now());
-      table.timestamp("updated_at").defaultTo(knex.fn.now());
+    knex.schema.hasTable("contacts").then(function(exists) {
+      if (!exists) {
+        return knex.schema.createTable("contacts", function(table) {
+          table.uuid("contact_id").primary();
+          table.string("display_name");
+          table.string("account_name").notNullable();
+          table.string("agency_number").notNullable();
+          table.string("mail_to_city");
+          table.string("mail_to_state");
+          table.string("mail_to_street");
+          table.string("mail_to_postal_code", 5);
+          table.string("first_name").notNullable();
+          table.string("last_name").notNullable();
+          table.string("dd", 2);
+          table.string("mm", 2);
+          table.string("yyyy", 4);
+          table.date("dob").notNullable();
+          table.string("preferred_language").notNullable();
+          table.string("home_street").notNullable();
+          table.string("home_postal_code", 5).notNullable();
+          table.string("home_state").notNullable();
+          table.string("home_city").notNullable();
+          table.string("home_email").notNullable();
+          table.string("mobile_phone").notNullable();
+          table.boolean("text_auth_opt_out").notNullable();
+          table.boolean("terms_agree").notNullable();
+          table.string("signature").notNullable();
+          table.string("online_campaign_source");
+          table.boolean("signed_application");
+          table.string("ethnicity");
+          table.boolean("lgbtq_id");
+          table.boolean("trans_id");
+          table.boolean("disability_id");
+          table.boolean("deaf_or_hard_of_hearing");
+          table.boolean("blind_or_visually_impaired");
+          table.string("gender");
+          table.string("gender_other_description");
+          table.string("gender_pronoun");
+          table.string("job_title");
+          table.date("hire_date");
+          table.string("worksite");
+          table.string("work_email");
+          table.timestamp("created_at").defaultTo(knex.fn.now());
+          table.timestamp("updated_at").defaultTo(knex.fn.now());
+        });
+      }
     })
   ]);
 };
 
 exports.down = function(knex, Promise) {
-  return Promise.all([knex.schema.dropTable("submissions")]);
+  return Promise.all([
+    knex.schema.hasTable("contacts").then(function(exists) {
+      if (exists) {
+        return knex.schema.dropTable("contacts");
+      }
+    })
+  ]);
 };
--- db/migrations/testing/20190605114820_rename_content_table.js
+++ db/migrations/testing/20190605114820_rename_content_table.js
@@ -1,17 +1,31 @@
 exports.up = function(knex, Promise) {
   return Promise.all([
-    knex.schema.renameTable("form_meta", "content"),
-    knex.schema.alterTable("content", function(t) {
-      t.renameColumn("form_meta_type", "content_type");
+    knex.schema.hasColumn("form_meta", "form_meta_type").then(function(exists) {
+      if (exists) {
+        return knex.schema.table("form_meta", function(t) {
+          t.renameColumn("form_meta_type", "content_type");
+        });
+      }
+    }),
+    knex.schema.hasTable("form_meta").then(function(exists) {
+      if (exists) {
+        return knex.schema.renameTable("form_meta", "content");
+      }
     })
   ]);
 };
 
 exports.down = function(knex, Promise) {
   return Promise.all([
-    knex.schema.alterTable("content", function(t) {
-      t.renameColumn("content_type", "form_meta_type");
-    }),
-    knex.schema.renameTable("content", "form_meta")
+    knex.schema.hasTable("content").then(function(exists) {
+      if (exists) {
+        return (
+          knex.schema.alterTable("content", function(t) {
+            t.renameColumn("content_type", "form_meta_type");
+          }),
+          knex.schema.renameTable("content", "form_meta")
+        );
+      }
+    })
   ]);
 };
