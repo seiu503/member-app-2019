fa22c4c84991517ec4754c8e5c9dee6e45747cfc
rifkegribenes <rifkegribenes@gmail.com>
2019-08-22 10:49:38 -0700

    refactored image upload, promisified multer wrapper

--- app/controllers/image.ctrl.js
+++ app/controllers/image.ctrl.js
@@ -4,131 +4,79 @@
 
 /* ================================= SETUP ================================= */
 
-// import model
-const content = require("../../db/models/content");
-
-const aws = require("aws-sdk");
-const multerS3 = require("multer-s3");
 const multer = require("multer");
-const MulterWrapper = require("../utils/multer.js");
-const path = require("path");
-const url = require("url");
-
+const content = require("../../db/models/content");
+const { Uploader } = require("../utils/multer.js");
+const uploader = new Uploader();
 const s3config = require("../config/aws");
-let S3 = new aws.S3(s3config);
-
-/**
- * Upload to s3 bucket with multer
- */
-exports.upload = MulterWrapper.multer({
-  storage: multerS3({
-    s3: S3,
-    bucket: s3config.bucket,
-    acl: "public-read",
-    key: function(req, file, cb) {
-      cb(
-        null,
-        path.basename(
-          file.originalname,
-          `${path.extname(file.originalname)}-${Date.now()}${path.extname(
-            file.originalname
-          )}`
-        )
-      );
-    }
-  }),
-  limits: { fileSize: 2000000 }, // In bytes: 2000000 bytes = 2 MB
-  fileFilter: function(req, file, cb) {
-    exports.checkFile(file, cb);
-  }
-}).single("image");
-
-/**
- * Check File Type
- * @param file
- * @param cb
- * @return {*}
- */
-exports.checkFile = (file, cb) => {
-  console.log(`image.ctrl.js > 53`);
-  console.log(file);
-  // Allowed ext
-  const filetypes = /jpeg|jpg|png|gif/;
-  // Check ext
-  const extname = filetypes.test(path.extname(file.originalname).toLowerCase());
-  // Check mime
-  const mimetype = filetypes.test(file.mimetype);
-  if (mimetype && extname) {
-    return cb(null, true);
-  } else {
-    return cb({
-      message: "Error: Only jpeg, jpg, png, and gif files accepted."
-    });
-  }
-};
 
 /** Upload a single image
  *  @param    {File}   file            Uploaded file.
  *  @returns  {Object}                 Image name and URL OR error message.
  */
 exports.singleImgUpload = async (req, res, next) => {
-  const result = await exports.upload(req, res, next);
   // upload image to s3 bucket
+  console.log(`image.ctrl.js > 20`);
 
-  // if (!req.file) {
-  //   console.log(`image.ctrl.js > 92`);
-  //   console.log("No file found");
-  //   return res.status(500).json({
-  //     message: "No file attached. Please choose a file."
-  //   });
-  // }
-  if (result instanceof multer.MulterError) {
-    console.log(`image.ctrl.js > 78`);
-    console.log(result);
-    return res.status(500).json({
-      message: result.message
-    });
-  }
-  if (result instanceof Error) {
-    console.log(`image.ctrl.js > 85`);
-    console.log(result);
-    return res.status(500).json({
-      message: result.message
-    });
-  }
-
-  // generate url of uploaded image
-  const imageUrl = `https://${s3config.bucket}.s3-${
-    s3config.region
-  }.amazonaws.com/${req.file.originalname}`;
-  // check if apiCall is for admin image or user signature image
-  if (req.file.originalname.includes("__signature__")) {
-    return res.status(200).json(imageUrl);
-  }
-  // check if we're creating a new DB record or updating existing
-  if (req.body.id) {
-    // update existing record
-    const updates = {
-      content_type: "image",
-      content: imageUrl
-    };
-    try {
-      const records = await content.updateContent(req.body.id, updates);
-      return res.status(200).json(records[0]);
-    } catch (err) {
-      console.log(`imageUpload.ctrl.js > 109: ${err}`);
+  uploader
+    .startUpload(req, res, next)
+    .then(() => {
+      // console.log(`image.ctrl.js > 77`);
+      // console.log(req.file);
+      if (!req.file) {
+        console.log(`image.ctrl.js > 27`);
+        console.log("No file found");
+        return res.status(500).json({
+          message: "No file attached. Please choose a file."
+        });
+      }
+      // generate url of uploaded image
+      const imageUrl = `https://${s3config.bucket}.s3-${
+        s3config.region
+      }.amazonaws.com/${req.file.originalname}`;
+      // check if apiCall is for admin image or user signature image
+      if (req.file.originalname.includes("__signature__")) {
+        return res.status(200).json(imageUrl);
+      }
+      // check if we're creating a new DB record or updating existing
+      if (req.body.id) {
+        // update existing record
+        const updates = {
+          content_type: "image",
+          content: imageUrl
+        };
+        content
+          .updateContent(req.body.id, updates)
+          .then(records => {
+            return res.status(200).json(records[0]);
+          })
+          .catch(err => {
+            console.log(`imageUpload.ctrl.js > 53: ${err}`);
+            return res.status(500).json({ message: err.message });
+          });
+      } else {
+        // create new record in postgres DB
+        content
+          .newContent("image", imageUrl)
+          .then(records => {
+            return res.status(200).json(records[0]);
+          })
+          .catch(err => {
+            console.log(`image.ctrl.js > 64: ${err}`);
+            return res.status(500).json({ message: err.message });
+          });
+      }
+    })
+    .catch(err => {
+      if (err instanceof multer.MulterError) {
+        console.log(`image.ctrl.js > 71: ${err}`);
+        return res.status(500).json({
+          message: err.message
+        });
+      }
+      console.log(`image.ctrl.js > 76: ${err}`);
       return res.status(500).json({ message: err.message });
-    }
-  } else {
-    // create new record in postgres DB
-    try {
-      const records = await content.newContent("image", imageUrl);
-      return res.status(200).json(records[0]);
-    } catch (err) {
-      console.log(`image.ctrl.js > 125: ${err}`);
-      return res.status(500).json({ message: err.message });
-    }
-  }
+    });
 };
 
 /**
@@ -141,9 +89,9 @@ exports.deleteImage = async (req, res, next) => {
     await S3.deleteObject(params);
     return res.status(200).json({ message: "Image deleted." });
   } catch (err) {
-    // console.log(err);
+    // console.log(`image.ctrl.js > 91: ${err});
     return res.status(500).json({
-      message: err
+      message: err.message
     });
   }
 };
--- app/utils/multer.js
+++ app/utils/multer.js
@@ -1,5 +1,75 @@
-// local module to wrap multer so that it can be stubbed with sinon
-// for testing image upload route:
-
 const multer = require("multer");
-module.exports = { multer };
+const util = require("util");
+const multerS3 = require("multer-s3");
+const path = require("path");
+const aws = require("aws-sdk");
+const s3config = require("../config/aws");
+let S3 = new aws.S3(s3config);
+
+const checkFile = (file, cb) => {
+  console.log(`multer.js > 32`);
+  console.log(file);
+  // Allowed ext
+  const filetypes = /jpeg|jpg|png|gif/;
+  // Check ext
+  const extname = filetypes.test(path.extname(file.originalname).toLowerCase());
+  // Check mime
+  const mimetype = filetypes.test(file.mimetype);
+  if (mimetype && extname) {
+    return cb(null, true);
+  } else {
+    return cb({
+      message: "Error: Only jpeg, jpg, png, and gif files accepted."
+    });
+  }
+};
+
+class Uploader {
+  constructor() {
+    const storageOptions = multerS3({
+      s3: S3,
+      bucket: s3config.bucket,
+      acl: "public-read",
+      key: function(req, file, cb) {
+        cb(
+          null,
+          path.basename(
+            file.originalname,
+            `${path.extname(file.originalname)}-${Date.now()}${path.extname(
+              file.originalname
+            )}`
+          )
+        );
+      }
+    });
+
+    this.upload = multer({
+      storage: storageOptions,
+      limits: { fileSize: 2000000 }, // In bytes: 2000000 bytes = 2 MB
+      fileFilter: function(req, file, cb) {
+        checkFile(file, cb);
+      }
+    });
+  }
+
+  async startUpload(req, res, next) {
+    let filename;
+    console.log("multer 60");
+    try {
+      const upload = util.promisify(this.upload.single("image"));
+      await upload(req, res);
+      filename = req.file.filename;
+      console.log("multer 65");
+    } catch (err) {
+      console.log(`multer.js > 66: ${err}`);
+      return res.status(500).json({ message: err.message });
+    }
+    console.log("multer 70");
+    return;
+  }
+}
+
+module.exports = {
+  Uploader,
+  checkFile
+};
