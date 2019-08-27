8d78d992751fd6620079dfc5b2f9c89cdd222355
Jordan Heffernan <jheff2@gmail.com>
2019-08-27 08:43:17 -0700

    added languageToggle component

--- client/src/containers/NavBar.jsx
+++ client/src/containers/NavBar.jsx
@@ -17,6 +17,7 @@ import Slide from "@material-ui/core/Slide";
 
 import { skip } from "../utils";
 import logo from "../img/seiu503_white.svg";
+import LanguageToggle from "../components/LanguageToggle";
 
 const styles = theme => ({
   root: {
@@ -255,6 +256,7 @@ export class NavBar extends React.Component {
                 Membership Application
               </Link>
             </Typography>
+            <LanguageToggle />
             <IconButton
               className={classes.menuButton}
               color="secondary"
