8999e92030f588ba203efe501db1ce2e1a2d8c54
rifkegribenes <rifkegribenes@gmail.com>
2019-09-04 15:22:32 -0700

    trying again with submission table migration file

--- db/migrations/testing/20190705143327_add_salesforce_id_rename_primary.js
+++ db/migrations/testing/20190705143327_add_salesforce_id_rename_primary.js
@@ -2,23 +2,11 @@ exports.up = function(knex) {
   return Promise.all([
     knex.schema.hasTable("submissions").then(function(exists) {
       if (exists) {
-        return knex.schema
-          .hasColumn("submissions", "salesforce_id")
-          .then(function(exists) {
-            if (!exists) {
-              return knex.schema.table("submissions", function(table) {
-                return knex.schema
-                  .hasColumn("submissions", "submission_id")
-                  .then(function(exists) {
-                    if (exists) {
-                      table.uuid("salesforce_id").notNullable().unique;
-                      table.renameColumn("submission_id", "id");
-                      table.dropColumn("contact_id");
-                    }
-                  });
-              });
-            }
-          });
+        return knex.schema.table("submissions", function(table) {
+          table.uuid("salesforce_id").notNullable().unique;
+          table.renameColumn("submission_id", "id");
+          table.dropColumn("contact_id");
+        });
       }
     })
   ]);
