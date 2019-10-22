20c020284ee63bfcc2a4c3568796f59c61c2424c
rifkegribenes <rifkegribenes@gmail.com>
2019-10-22 16:54:49 -0700

    fixed directory in postInstall.sh

--- ec2/scripts/postInstall.sh
+++ ec2/scripts/postInstall.sh
@@ -5,11 +5,11 @@
 # and production build to production db
 start=$SECONDS
 sudo chown -R ec2-user /home/ubuntu/seiu503Deployed
+cd client && yarn install
 npm install -g env-path
 nohup env-path -p .env.staging yarn node node_modules/react-scripts/scripts/build.js
 knex migrate:latest --env production
 yarn install
-cd client && yarn install
 pm2 restart 0 --time --update-env --merge-logs
 end=$SECONDS
 echo "postInstall.sh script finished"
