348054632920cba98a1591725d437318c607485a
rifkegribenes <rifkegribenes@gmail.com>
2019-10-22 16:39:45 -0700

    remove staging build step from travis, add to postInstall.sh

--- ec2/scripts/postInstall.sh
+++ ec2/scripts/postInstall.sh
@@ -3,12 +3,14 @@
 # running database migrations for production database
 # need to update this to match staging build to staging db
 # and production build to production db
+start=$SECONDS
 sudo chown -R ec2-user /home/ubuntu/seiu503Deployed
-yarn install
-cd client && yarn install
 npm install -g env-path
 nohup env-path -p .env.staging yarn node node_modules/react-scripts/scripts/build.js
 knex migrate:latest --env production
+yarn install
+cd client && yarn install
 pm2 restart 0 --time --update-env --merge-logs
-
+end=$SECONDS
 echo "postInstall.sh script finished"
+echo "duration: $((end-start)) seconds."
