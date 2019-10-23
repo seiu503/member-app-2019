db46cc3bf8c226df6b0befd34ec6ae9312ce4116
rifkegribenes <rifkegribenes@gmail.com>
2019-10-23 12:41:10 -0700

    add changes to travis & postInstall scripts

--- ec2/scripts/postInstall.sh
+++ ec2/scripts/postInstall.sh
@@ -1,9 +1,21 @@
 #!/bin/bash
+# this is running a staging build but
+# running database migrations for production database
+# need to update this to match staging build to staging db
+# and production build to production db
+start=$SECONDS
+destdir=/home/ubuntu/seiu503Deployed/ec2/scripts/scriptlog.txt
+thedatetime=$(date +%d-%m-%Y" "%H:%M:%S)
 sudo chown -R ec2-user /home/ubuntu/seiu503Deployed
+sudo chown -R ec2-user /home/ubuntu/seiu503Deployed/ec2/scripts/scriptlog.txt
+sudo chmod 777 /home/ubuntu/seiu503Deployed/ec2/scripts/scriptlog.txt
 yarn install
-cd client && yarn install
-yarn node node_modules/react-scripts/scripts/build.js
 knex migrate:latest --env production
-pm2 restart 0 --update-env
-
+pm2 restart 0 --time --update-env --merge-logs
+end=$SECONDS
 echo "postInstall.sh script finished"
+echo "duration: $((end-start)) seconds."
+if [ -f "$destdir" ]
+then
+    printf '%b\n' "$thedatetime duration: $((end-start)) seconds." >> "$destdir"
+fi
