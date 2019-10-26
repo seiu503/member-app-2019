589924bc4563f949a217e0f57bb69a6c3f9d284f
rifkegribenes <rifkegribenes@gmail.com>
2019-10-26 15:20:21 -0700

    update script path in appspec"

--- ec2/scripts/postInstall.sh
+++ ec2/scripts/postInstall.sh
@@ -1,15 +1,10 @@
 #!/bin/bash
-# this is running a staging build but
-# running database migrations for production database
-# need to update this to match staging build to staging db
-# and production build to production db
 start=$SECONDS
 destdir=/home/ubuntu/seiu503Deployed/ec2/scripts/scriptlog.txt
 thedatetime=$(date +%d-%m-%Y" "%H:%M:%S)
 sudo chown -R ec2-user /home/ubuntu/seiu503Deployed
 sudo chown -R ec2-user /home/ubuntu/seiu503Deployed/ec2/scripts/scriptlog.txt
 sudo chmod 777 /home/ubuntu/seiu503Deployed/ec2/scripts/scriptlog.txt
-yarn install
 knex migrate:latest --env production
 pm2 restart 0 --time --update-env --merge-logs
 end=$SECONDS
