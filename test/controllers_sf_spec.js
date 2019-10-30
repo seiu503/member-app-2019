c9f7cdd587505c596fb28e1a8405290c09ea859e
rifkegribenes <rifkegribenes@gmail.com>
2019-10-30 15:31:25 -0700

    new controller test for getSFCAPEByContactId

--- test/controllers_sf_spec.js
+++ test/controllers_sf_spec.js
@@ -1709,4 +1709,77 @@ suite("sf.ctrl.js", function() {
       });
     });
   });
+
+  suite("sfCtrl > getSFCAPEByContactId", function() {
+    beforeEach(function() {
+      return new Promise(resolve => {
+        req = mockReq({
+          params: {
+            id: "123456789"
+          }
+        });
+        responseStub = { records: [contactStub] };
+        queryStub = sinon.stub().returns((null, responseStub));
+        loginStub = sinon.stub();
+        jsforceStub = {
+          login: loginStub,
+          query: queryStub
+        };
+        jsforceConnectionStub = sinon
+          .stub(jsforce, "Connection")
+          .returns(jsforceStub);
+        resolve();
+      });
+    });
+
+    afterEach(() => {
+      sinon.restore();
+    });
+
+    test("gets a single CAPE record by Contact Id", async function() {
+      query = `SELECT Active_Account_Last_4__c, Payment_Error_Hold__c, Unioni_se_MemberID__c, Card_Brand__c, LastModifiedDate, Id, Employer__c FROM CAPE__c WHERE Worker__c = \'123456789\' ORDER BY LastModifiedDate DESC LIMIT 1`;
+      try {
+        await sfCtrl.getSFCAPEByContactId(req, res);
+        assert.called(jsforceConnectionStub);
+        assert.called(jsforceStub.login);
+        assert.calledWith(jsforceStub.query, query);
+        assert.calledWith(res.json, contactStub);
+      } catch (err) {
+        console.log(err);
+      }
+    });
+
+    test("returns error if login fails", async function() {
+      loginError =
+        "Error: INVALID_LOGIN: Invalid username, password, security token; or user locked out.";
+      loginStub = sinon.stub().throws(new Error(loginError));
+      jsforceStub.login = loginStub;
+
+      try {
+        await sfCtrl.getSFCAPEByContactId(req, res);
+        assert.called(jsforceConnectionStub);
+        assert.called(jsforceStub.login);
+        assert.calledWith(res.status, 500);
+        assert.calledWith(res.json, { message: loginError });
+      } catch (err) {
+        console.log(err);
+      }
+    });
+
+    test("returns error if query fails", async function() {
+      queryError = "Error: MALFORMED_QUERY: unexpected token: query";
+      queryStub = sinon.stub().throws(new Error(queryError));
+      jsforceStub.query = queryStub;
+
+      try {
+        await sfCtrl.getSFCAPEByContactId(req, res);
+        assert.called(jsforceConnectionStub);
+        assert.called(jsforceStub.query);
+        assert.calledWith(res.status, 500);
+        assert.calledWith(res.json, { message: queryError });
+      } catch (err) {
+        console.log(err);
+      }
+    });
+  });
 });
