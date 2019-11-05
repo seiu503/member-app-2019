07e97b35bfd8cf6b38b98de60ea59889e8a448b6
rifkegribenes <rifkegribenes@gmail.com>
2019-11-05 13:03:45 -0800

    fix postgres config for knex

--- knexfile.js
+++ knexfile.js
@@ -34,12 +34,7 @@ module.exports = {
   },
   devstaging: {
     client: "pg",
-    connection: {
-      host: process.env.STAGING_DATABASE_ENDPOINT,
-      user: process.env.STAGING_DATABASE_USER,
-      password: process.env.STAGING_DATABASE_PASSWORD,
-      database: process.env.STAGING_DATABASE_NAME
-    },
+    connection: process.env.STAGING_DATABASE_ENDPOINT,
     migrations: {
       directory: "./db/migrations/staging"
     },
