e90cb6dd78f1336cb69e3be0d3bd576e702d2842
rifkegribenes <rifkegribenes@gmail.com>
2019-11-15 09:41:08 -0800

    fix text auth opt out issue

--- app/controllers/sf.ctrl.js
+++ app/controllers/sf.ctrl.js
@@ -145,11 +145,11 @@ exports.getSFContactByDoubleId = async (req, res, next) => {
  *  @returns  {Object}        { sf_contact_id } or error message
  */
 exports.createSFContact = async (req, res, next) => {
-  // console.log(`sf.ctrl.js > 62: createSFContact`);
+  console.log(`sf.ctrl.js > 148: createSFContact`);
 
   const bodyRaw = { ...req.body };
-  // console.log(`sf.ctrl.js > 64`);
-  // console.log(bodyRaw);
+  console.log(`sf.ctrl.js > 151`);
+  console.log(bodyRaw);
   const body = {};
 
   // convert raw body to key/value pairs using SF API field names
@@ -165,7 +165,8 @@ exports.createSFContact = async (req, res, next) => {
   delete body["Account.Agency_Number__c"];
   delete body["Account.WS_Subdivision_from_Agency__c"];
   body.AccountId = bodyRaw.employer_id;
-
+  console.log("sf.ctrl.js > 168");
+  console.log(body);
   let conn = new jsforce.Connection({ loginUrl });
   try {
     await conn.login(user, password);
