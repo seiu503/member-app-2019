c505c1938ef5e5f59f0d7350d6beb9bdcd7b93a7
rifkegribenes <rifkegribenes@gmail.com>
2020-05-19 14:35:15 -0700

    missing fields added to OMA

--- app/utils/index.js
+++ app/utils/index.js
@@ -6,6 +6,9 @@ const https = require("https");
 const sfCtrl = require("../controllers/sf.ctrl");
 const submissionCtrl = require("../controllers/submissions.ctrl");
 
+// updated May 18, 2020, PS only (for no-js form)
+const legal_language = `<div><h3>Membership Authorization</h3><p><strong>Yes, I want to join with my fellow employees and become a member of SEIU Local 503 (“Local 503”).</strong>This means I will receive the benefits and abide by the obligations of membership set forth in the Constitutions and Bylaws of both Local 503 and the Service Employees International Union (“SEIU”). I authorize Local 503 to act as my representative in collective bargaining over wages, benefits, and other terms and conditions of employment with my employer, and as my exclusive representative where authorized by law. I know that membership in the union is voluntary and is not a condition of my employment, and that I can decline to join without reprisal.</p><h3>Dues Deduction/Checkoff Authorization</h3><p>I request and voluntarily authorize my employer to deduct from my earnings and to pay to Local 503 an amount equal to the regular monthly dues and other fees or assessments uniformly applicable to members of Local 503. This authorization shall remain in effect unless I revoke it by sending written notice via U.S. mail to Local 503 during the periods not less than 30 days and not more than 45 days before either (1) the annual anniversary date of this agreement, or (2) the date of termination of the applicable collective bargaining agreement between the employer and Local 503. This authorization shall be automatically renewed from year to year unless I revoke it in writing during a window period, even if I have resigned my membership.</p><p>This authorization is voluntary and is not a condition of my employment, and I can decline to agree to it without reprisal. I understand that all members benefit from everyone’s commitments because they help to build a strong union that is able to plan for the future.</p></div>`;
+
 const CLIENT_URL =
   process.env.NODE_CONFIG_ENV === "production"
     ? process.env.APP_HOST_PROD
@@ -75,20 +78,22 @@ formatSFDate = date => {
   return [year, month, day].join("-");
 };
 
-/** Handle Tab1 Tab2 submissions from noscript form */
+/** Handle Tab1 & Tab2 submissions from noscript form */
 // for users with javascript disabled, all front-end processing
-// has to be moved to back end
+// moved to back end
 
 // handleTab1 performs:
 // lookupSFContactByFLE
 // getSFEmployers
 // createSFContact
 // updateSFContact
-// createSumbission <== ???
+// createSumbission
 const handleTab1 = async (req, res, next) => {
   req.body.birthdate = formatSFDate(req.body.birthdate);
   if (!req.body.text_auth_opt_out) {
     req.body.text_auth_opt_out = false;
+  } else if (req.body.text_auth_opt_out) {
+    req.body.text_auth_opt_out = true;
   }
   const formValues = { ...req.body };
   console.log(`utils/index.js > handleTab1 94: formValues`);
@@ -214,18 +219,21 @@ const handleTab2 = async (req, res, next) => {
   submissionCtrl
     .updateSubmission(req, res, next)
     .then(submissionBody => {
-      console.log(`@@@@ Is this the updated submission body?`);
-      console.log(submissionBody);
       req.body = { ...formValues, ...submissionBody };
-      console.log(`utils.index.js > 210 handleTab2: req.body`);
+      req.body.online_campaign_source = "NoJavascript";
+      req.body.legal_language = legal_language;
+      req.body.maintenance_of_effort = formatSFDate(new Date());
+      req.body.seiu503_cba_app_date = formatSFDate(new Date());
+      req.body.immediate_past_member_status = "Not a Member";
+      console.log(`utils.index.js > 228 handleTab2: req.body`);
       console.log(req.body);
       sfCtrl.createSFOnlineMemberApp(req, res, next).then(sf_OMA_id => {
-        console.log(`utils/index.js > 211 handleTab2 sfOMA success`);
+        console.log(`utils/index.js > 231 handleTab2 sfOMA success`);
         return res.redirect("https://seiu503.org/members/thank-you/");
       });
     })
     .catch(err => {
-      console.error(`utils/index.js > handleTab2 209: ${err}`);
+      console.error(`utils/index.js > handleTab2 236: ${err}`);
       return handleError(err);
     });
 };
