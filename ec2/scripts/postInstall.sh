d306bb453f4d78853d3d0467b5739f278198a568
rifkegribenes <rifkegribenes@gmail.com>
2020-05-22 17:03:13 -0700

    add knex install to postinstall script

--- ec2/scripts/postInstall.sh
+++ ec2/scripts/postInstall.sh
@@ -5,6 +5,7 @@ thedatetime=$(date +%d-%m-%Y" "%H:%M:%S)
 sudo chown -R ec2-user /home/ubuntu/seiu503Deployed
 sudo chown -R ec2-user /home/ubuntu/seiu503Deployed/ec2/scripts/scriptlog.txt
 sudo chmod 777 /home/ubuntu/seiu503Deployed/ec2/scripts/scriptlog.txt
+sudo npm install knex -g
 knex migrate:latest --env production
 pm2 restart 0 --time --update-env --merge-logs --log-date-format "YYYY-MM-DD HH:mm" --watch
 at -M now + 2 minute <<< $'sudo service codedeploy-agent restart'
