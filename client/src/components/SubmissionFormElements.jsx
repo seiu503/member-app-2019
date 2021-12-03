f25497c389bc91883040b1d8694f7cb0ca68d82d
rifkegribenes <rifkegribenes@gmail.com>
2021-12-03 11:38:42 -0800

    add farsi to language options

--- client/src/components/SubmissionFormElements.jsx
+++ client/src/components/SubmissionFormElements.jsx
@@ -169,6 +169,7 @@ export const languageOptions = [
   "Vietnamese",
   "Amharic",
   "Arabic",
+  "Farsi",
   "Haitian Creole"
 ];
 export const genderOptions = ["", "Female", "Male", "Non-Binary", "Other"];
