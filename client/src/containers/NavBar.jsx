28d6c24ddde75f2232db0542bfb8698b4ca1aa56
sarah <rifkegribenes@gmail.com>
2023-01-13 16:04:23 -0800

    removed commented styles from NavBar

--- client/src/containers/NavBar.jsx
+++ client/src/containers/NavBar.jsx
@@ -24,158 +24,6 @@ import { skip } from "../utils";
 import { LanguagePicker } from "../components/SubmissionFormElements";
 import logo from "../img/seiu503_white.svg";
 
-// const styles = theme => ({
-//   root: {
-//     flexGrow: 1,
-//     width: "100vw",
-//     color: "#2c0940" // dark purple // theme.palette.primary.main,
-//   },
-//   flex: {
-//     flexGrow: 1
-//   },
-//   appBar: {
-//     backgroundColor: "#2c0940", // dark purple // theme.palette.primary.main,
-//     position: "fixed"
-//   },
-//   menuButton: {
-//     display: "none",
-//     [theme.breakpoints.down("sm")]: {
-//       display: "block",
-//       position: "absolute",
-//       right: 20
-//     }
-//   },
-//   menuWrap: {
-//     [theme.breakpoints.down("sm")]: {
-//       display: "none"
-//     }
-//   },
-//   menuLink: {
-//     color: "#ffce04", // yellow/gold // theme.palette.secondary.main,
-//     textTransform: "capitalize",
-//     fontSize: "1em"
-//   },
-//   title: {
-//     flexGrow: 1,
-//     color: "#ffffff", // white // theme.palette.secondary.light,
-//     fontFamily: '"Source Sans Pro", sans-serif',
-//     fontSize: "1.7em",
-//     textDecoration: "none",
-//     paddingLeft: 10,
-//     fontWeight: 200,
-//     [theme.breakpoints.down("md")]: {
-//       fontSize: "1.1rem",
-//       fontWeight: 400
-//     },
-//     [theme.breakpoints.down("xs")]: {
-//       display: "none"
-//     }
-//   },
-//   loginButton: {
-//     textDecoration: "none"
-//   },
-//   avatar: {
-//     marginRight: 20,
-//     [theme.breakpoints.down("xs")]: {
-//       display: "none"
-//     }
-//   },
-//   admin: {
-//     display: "flex"
-//   },
-//   skip: {
-//     position: "absolute",
-//     top: "-1000px",
-//     left: "-1000px",
-//     height: "1px",
-//     width: "1px",
-//     textAlign: "left",
-//     overflow: "hidden",
-
-//     "&:focus": {
-//       position: "relative",
-//       top: 0,
-//       left: "-13px",
-//       width: "auto",
-//       height: "auto",
-//       overflow: "visible",
-//       textAlign: "center",
-//       zIndex: "1000"
-//     }
-//   },
-//   menuItem: {
-//     padding: "24px 16px",
-//     textAlign: "center",
-//     textTransform: "capitalize",
-//     fontFamily: [
-//       '"Source Sans Pro"',
-//       '"Helvetica Neue"',
-//       "Helvetica",
-//       "Arial",
-//       "sans-serif"
-//     ].join(","),
-//     fontWeight: 400,
-//     color: "#ffce04" // yellow/gold // theme.palette.secondary.main,
-//   },
-//   ListItemText: {
-//     color: "#ffce04" // yellow/gold // theme.palette.secondary.main,
-//   },
-//   logo: {
-//     height: 60,
-//     width: "auto",
-//     marginRight: 10,
-//     [theme.breakpoints.down("sm")]: {
-//       height: 40
-//     }
-//   },
-//   drawer: {
-//     boxShadow:
-//       "inset 0px 2px 4px -1px rgba(0,0,0,.2), inset 0px -2px 4px -1px rgba(0,0,0,.2), inset 0px 4px 5px 0px rgba(0, 0, 0, 0.14), inset 0px -4px 5px 0px rgba(0, 0, 0, 0.14), inset 0px 1px 10px 0px rgba(0, 0, 0, 0.12), inset 0px -1px 10px 0px rgba(0, 0, 0, 0.12), -4px 0px 10px -2px rgba(0,0,0,.2)"
-//   },
-//   languagePicker: {
-//     color: "white",
-//     [theme.breakpoints.down("sm")]: {
-//       position: "absolute",
-//       top: 20,
-//       right: 10
-//     }
-//   },
-//   languagePickerLabel: {
-//     color: "white",
-//     marginTop: -14,
-//     marginLeft: 25
-//   },
-//   languagePickerSelect: {
-//     color: "white",
-//     width: 195,
-//     border: "1px solid white",
-//     paddingLeft: "7px !important"
-//   },
-//   lpInput: {
-//     // height: 30,
-//   },
-//   icon: {
-//     display: "none"
-//     // fill: 'white',
-//   },
-//   notched: {
-//     borderColor: "transparent"
-//     // borderRadius: 4,
-//     // "&:hover": "white",
-//     // "&:focused": "white",
-//   },
-//   labelShrink: {
-//     color: "white"
-//   },
-//   labelFocused: {
-//     color: "white !important",
-//     marginTop: -8
-//   },
-//   adornedStart: {
-//     paddingLeft: 10
-//   }
-// });
-
 export class NavBar extends React.Component {
   constructor(props) {
     super(props);
@@ -231,8 +79,7 @@ export class NavBar extends React.Component {
               "sans-serif"
             ].join(","),
             fontWeight: 400,
-            color: "#ffce04" // yellow/gold // theme.palette.secondary.main,
-            //className={classes.menuItem}
+            color: "secondary.main" // "#ffce04" // yellow/gold
           }}
           data-testid="menu-item-mobile"
         >
@@ -267,7 +114,7 @@ export class NavBar extends React.Component {
             <Button
               key={index}
               sx={{
-                color: "#ffce04", // yellow/gold // theme.palette.secondary.main,
+                color: "secondary.main", // "#ffce04", // yellow/gold
                 textTransform: "capitalize",
                 fontSize: "1em"
                 // className={classes.menuLink}
@@ -286,13 +133,13 @@ export class NavBar extends React.Component {
         sx={{
           flexGrow: 1,
           width: "100vw",
-          color: "#2c0940" // dark purple // theme.palette.primary.main,
+          color: "primary.main" // "#2c0940" // dark purple
           // className={classes.root}
         }}
       >
         <Box
           sx={{
-            backgroundColor: "#2c0940", // dark purple // theme.palette.primary.main,
+            backgroundColor: "primary.main", // "#2c0940", // dark purple
             position: "fixed"
             // className={classes.appBar}
           }}
@@ -364,7 +211,7 @@ export class NavBar extends React.Component {
                   to="/"
                   sx={{
                     flexGrow: 1,
-                    color: "#ffffff", // white // theme.palette.secondary.light,
+                    color: "secondary.light", // "#ffffff", // white
                     fontFamily: '"Source Sans Pro", sans-serif',
                     fontSize: {
                       xs: "1.1em",
@@ -427,18 +274,24 @@ export class NavBar extends React.Component {
                     onClick={e => this.handleClick(e)}
                     data-testid="menu-button"
                     sx={{
-                      display: "none",
-                      [theme.breakpoints.down("sm")]: {
-                        display: "block",
-                        position: "absolute",
-                        right: 20
-                        // className={classes.menuButton}
+                      display: {
+                        xs: "block",
+                        md: "none"
+                      },
+                      position: {
+                        xs: "absolute",
+                        md: "static"
+                      },
+                      right: {
+                        xs: 20,
+                        md: 0
                       }
                     }}
                   >
                     <MenuOutlined />
                   </IconButton>
-                  <Menu
+                  <Box
+                    element={Menu}
                     id="nav-menu"
                     anchorEl={anchorEl}
                     open={Boolean(anchorEl)}
@@ -458,17 +311,18 @@ export class NavBar extends React.Component {
                     }}
                   >
                     {mobileLinks}
-                  </Menu>
-                  <nav
+                  </Box>
+                  <Box
+                    element="nav"
                     sx={{
-                      [theme.breakpoints.down("sm")]: {
-                        display: "none"
+                      display: {
+                        xs: "none",
+                        md: "block"
                       }
-                      // className={classes.menuWrap}
                     }}
                   >
                     {adminMenuLinks}
-                  </nav>
+                  </Box>
                 </React.Fragment>
               )}
             </Toolbar>
@@ -503,5 +357,4 @@ const mapStateToProps = state => ({
   profile: state.profile
 });
 
-// export default withRouter(withStyles(styles)(connect(mapStateToProps)(NavBar)));
 export default withRouter(connect(mapStateToProps)(NavBar));
