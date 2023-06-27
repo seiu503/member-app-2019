b46d0c54036692ce57e0fd07e523ddaf5a0eef3c
sarah <rifkegribenes@gmail.com>
2023-06-27 12:07:53 -0700

    logging jest config to see if workerIdleMemoryLimit key is included

--- client/craco.config.js
+++ client/craco.config.js
@@ -3,8 +3,10 @@ module.exports = {
     configure: jestConfig => {
       jestConfig.coverageProvider = "v8";
       jestConfig.moduleDirectories = ["node_modules", "<rootDir>"];
+      jestConfig.testEnvironment = "jest-environment-jsdom-sixteen";
       jestConfig.workerIdleMemoryLimit = "512MB";
       jestConfig.isolatedModules = true;
+      console.log(jestConfig);
       return jestConfig;
     }
   }
